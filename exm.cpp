d2fe95ab71c9bc1c77e05a31b0506733c5da5fe3
Rulo123 <raul556496@gmail.com>
2017-10-30 11:18:11 -0600

    Actualizacion 1

--- exm.cpp
+++ exm.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 int main()
 {
+	int a, b, c;
 	cout<<"hola mundo";//LOL  xD   :v este es un comentario 
+	cout<<"dame un valor";
+	cin>>a;
+	cout<<"dame otro valor";
+	cin>>b;
+	c=a+b;
+	cout"el resultado es"c;
 	getch();
 }
